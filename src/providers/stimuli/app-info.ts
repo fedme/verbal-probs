293400d07e90aa75060924fd1fdae974ff8c8c18
Federico Meini <meini@mpib-berlin.mpg.de>
2018-07-26 13:36:43 +0200

    v1.1.1 - fixed intro layout

--- src/providers/stimuli/app-info.ts
+++ src/providers/stimuli/app-info.ts
@@ -1,5 +1,5 @@
 export abstract class AppInfo {
     static id: string = "252674c4";
     static nameLabel: string = "visual-probs";
-    static version: string = "1.1.0";
+    static version: string = "1.1.1";
 }
